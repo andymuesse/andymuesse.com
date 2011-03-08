ab29aa4618b7a5538ea5a233c985c766d04b7390
Andy Muesse <andy@Andy-Muesses-MacBook-Pro.local>
2011-03-07 19:29:36 -0800

    uploading framework

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd0ddf8b6c8354a09f8dec908825abb4f7f672843a35432d861925638e8fe91c203768b081cc9703371faee40873ad5b96f29c763391fb1dddb6c8d2e8b5c56cc'
